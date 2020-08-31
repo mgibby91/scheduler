615052f2cca65f5df9dcd70e0f56a1223f66a7ce
Matthew Gibson <mattgibson6@gmail.com>
2020-08-31 13:51:52 -0600

    built Button component and refactored using classnames module

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
-
 import "components/Button.scss";
 
+const classNames = require('classnames');
+
 export default function Button(props) {
-   return <></>;
+   // let buttonClass = 'button';
+
+   // if (props.confirm) {
+   //    buttonClass += ' button--confirm';
+   // } else if (props.danger) {
+   //    buttonClass += ' button--danger';
+   // }
+
+   const buttonClass = classNames('button', { 'button--confirm': props.confirm, 'button--danger': props.danger });
+
+   return (
+      <button
+         onClick={props.onClick}
+         disabled={props.disabled}
+         className={buttonClass}
+      >
+         {props.children}
+      </button >
+   );
 }
