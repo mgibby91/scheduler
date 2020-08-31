a17f85beac406ffdadeac46f26ea078bc7b932fd
Matthew Gibson <mattgibson6@gmail.com>
2020-08-31 17:20:55 -0600

    add interview list item component and storybook test

---
+++ src/components/InterviewListItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import "components/InterviewListItem.scss";
+
+const classNames = require('classnames');
+
+export default function InterviewListItem(props) {
+
+  const interviewClass = classNames(
+    'interviewers__item',
+    { 'interviewers__item--selected': props.selected },
+  );
+
+  const interviewImgClass = classNames(
+    'interviewers__item-image',
+    { 'interviewers__item--selected-image': props.selected }
+  );
+
+  let interviewName;
+
+  if (interviewClass === 'interviewers__item interviewers__item--selected') {
+    interviewName = props.name;
+  }
+
+  return (
+    <li className={interviewClass} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className={interviewImgClass}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {interviewName}
+    </li>
+  );
+
+
+}
