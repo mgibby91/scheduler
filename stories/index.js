8e8e2e546ea8f69efa5b609dccc5304ed0995e31
Matthew Gibson <mattgibson6@gmail.com>
2020-09-01 10:22:29 -0600

    empty, header, show and confirm appointment components all complete w stories

--- stories/index.js
+++ stories/index.js
@@ -4,13 +4,18 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import "index.scss";
+import 'components/Appointment/styles.scss';
 
 import Button from "components/Button";
 import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
-import Appointment from 'components/Appointment';
+import Appointment from 'components/Appointment/index';
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,11 +143,36 @@ storiesOf('Appointment', module)
   .addParameters({
     backgrounds: [{ name: 'white', value: '#fff', default: true }]
   })
-  .add('Appointment', () => {
-    <Appointment />
-  })
-  .add('Appointment with Time', () => {
+  .add('Appointment', () => (
+    < Appointment />
+  ))
+  .add('Appointment with Time', () => (
     <Appointment
       time={'12pm'}
     />
-  })
+  ))
+  .add('Header', () => (
+    <Header
+      time={'12pm'}
+    />
+  ))
+  .add('Empty', () => (
+    <Empty
+      onAdd={action('onAdd')}
+    />
+  ))
+  .add('Show', () => (
+    <Show
+      student={'Lydia Miller-Jones'}
+      interviewer={interviewer.name}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message={'Delete the appointment'}
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
+  ))
