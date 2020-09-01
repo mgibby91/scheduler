1a95119553e8e028fd66e5b7092ec3a67ba917f9
Matthew Gibson <mattgibson6@gmail.com>
2020-09-01 16:27:31 -0600

    update to Application component to include schedule data to pass down to Appointment to render

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DayList from 'components/DayList';
 import "components/Application.scss";
+import Appointment from 'components/Appointment/index'
 
 
 const days = [
@@ -21,9 +22,61 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+    interview: {
+      student: "Mike Tyson",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Tiger Woods",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "5pm",
+  },
+];
+
 export default function Application(props) {
 
   const [initialDay, setDay] = useState('Monday');
+
+  const appointmentData = appointments.map(appointment => {
+    return <Appointment key={appointment.id} {...appointment} />
+  })
+
 
   return (
     <main className="layout">
@@ -49,9 +102,12 @@ export default function Application(props) {
           />
         </section>
       }
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      {
+        <section className="schedule">
+          {appointmentData}
+          <Appointment key="last" time="6pm" />
+        </section>
+      }
     </main>
   );
 }
