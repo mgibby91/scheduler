bd41f663cd6f86bb24c7da76f970f7ef0c4aae53
Matthew Gibson <mattgibson6@gmail.com>
2020-09-02 21:49:15 +0000

    add rest of custom hook functions to useVisualMode and tested

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,13 +5,21 @@ export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(mode) {
+  function transition(mode, replace = false) {
+    if (replace) {
+      history[history.length - 1] = mode;
+      setHistory(history);
+    } else {
+      setHistory([...history, mode]);
+    }
     setMode(mode);
-    setHistory(mode);
   }
 
   function back() {
-    setMode(history);
+    if (history.length > 1) {
+      history.pop()
+    }
+    setMode(history[history.length - 1])
   }
 
   return {
